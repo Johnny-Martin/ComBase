2156963781ca293c3df10c3724e69494b1e4be87
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2015-12-09 23:05:34 +0800

    add comment

--- UIEngine/source/core/UIFrameWnd.cpp
+++ UIEngine/source/core/UIFrameWnd.cpp
@@ -11,7 +11,7 @@ bool FrameWnd::Initialize(HINSTANCE hInstance)
 	wcex.style         = CS_HREDRAW | CS_VREDRAW;
 	wcex.lpfnWndProc   = WndProc;
 	wcex.cbClsExtra    = 0;
-	wcex.cbWndExtra    = sizeof(LONG_PTR);		//为用户留4自节的指针存放区域.
+	wcex.cbWndExtra    = sizeof(LONG_PTR);		//extra window memory for saving 'this' pointer
 	wcex.hInstance     = hInstance;
 	wcex.lpszMenuName  = NULL;
 	wcex.hCursor       = LoadCursor(NULL, IDC_ARROW);
@@ -22,7 +22,7 @@ bool FrameWnd::Initialize(HINSTANCE hInstance)
 
 	if(!RegisterClassEx(&wcex))
 	{
-		WARNING_HWND_MSG(NULL, _T("注册窗口类失败!"));
+		WARNING_HWND_MSG(NULL, _T("register window failed!"));
 		return false;
 	}
 
@@ -38,13 +38,14 @@ bool FrameWnd::Initialize(HINSTANCE hInstance)
 		NULL,
 		NULL,
 		hInstance,
-		this			//保存this指针.
+		this  //be passed to the window through the CREATESTRUCT structure (lpCreateParams member),
+		      // CREATESTRUCT structure is pointed by the lParam param of the WM_CREATE message.
 		);
 
-	// 创建D2D资源.
+	// Create Dirct2D resource
 	if(!CreateDeviceIndependentResources() || !CreateDeviceDependentResources())
 	{
-		WARNING_HWND_MSG(m_hWnd, _T("创建Dirct2D资源失败!"));
+		WARNING_HWND_MSG(m_hWnd, _T("Create Dirct2D resource failed!"));
 		return FALSE;
 	}
 
@@ -76,7 +77,8 @@ bool FrameWnd::Initialize(HINSTANCE hInstance)
 		return false;
 	}
 
-	OnRender();				//窗体一显示出来就执行绘制.
+	//Render right now
+	OnRender();	
 
 	return TRUE;
 }
@@ -92,6 +94,9 @@ BOOL FrameWnd::CreateDeviceIndependentResources()
 	return SUCCEEDED(hr);
 }
 
+HDC MemDC;
+HBITMAP DIBSectionBitmap;
+HBITMAP OldBmp;
 BOOL FrameWnd::CreateDeviceDependentResources()
 {
 	RECT rc;
@@ -103,7 +108,7 @@ BOOL FrameWnd::CreateDeviceDependentResources()
 		);
 
 	//DXGI_FORMAT_R8G8B8A8_UNORM
-	// 创建D2D目标绘制区域.
+	// Create Dirct2D render target.
 	//DXGI_FORMAT_B8G8R8A8_UNORM
 	D2D1_PIXEL_FORMAT pixelFormat =  PixelFormat(DXGI_FORMAT_R8G8B8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED);
 	D2D1_RENDER_TARGET_PROPERTIES renderTargetProperties = RenderTargetProperties(D2D1_RENDER_TARGET_TYPE_DEFAULT,pixelFormat);
@@ -113,6 +118,30 @@ BOOL FrameWnd::CreateDeviceDependentResources()
 		HwndRenderTargetProperties(m_hWnd, size),
 		&m_pRenderTarget
 		);
+
+	//initialize the device context
+	RECT r = {0};
+	GetWindowRect(m_hWnd,&r);
+	HDC scrDC = GetDC(0);
+	MemDC = CreateCompatibleDC(scrDC);
+	ReleaseDC(0,scrDC);
+	if(!MemDC)
+	{
+		// FailInit();
+	}
+	BITMAPINFO bmi = {0};
+	bmi.bmiHeader.biBitCount = 32;
+	bmi.bmiHeader.biCompression = BI_RGB;
+	bmi.bmiHeader.biPlanes = 1;
+	bmi.bmiHeader.biWidth = r.right-r.left;
+	bmi.bmiHeader.biHeight = r.bottom-r.top;
+	bmi.bmiHeader.biSize = sizeof(bmi.bmiHeader);
+	DIBSectionBitmap = CreateDIBSection(MemDC,&bmi,DIB_RGB_COLORS,0,0,0);
+	if(!DIBSectionBitmap)
+		return 0;
+	OldBmp = (HBITMAP)SelectObject(MemDC,DIBSectionBitmap);
+	// Now create the HWND D2D1 RenderTarget.
+
 
 	//To enable the device context (DC) render target to work with GDI, set the DXGI format 
 	//to DXGI_FORMAT_B8G8R8A8_UNORM and the alpha mode to D2D1_ALPHA_MODE_PREMULTIPLIED or 
@@ -140,7 +169,9 @@ BOOL FrameWnd::CreateDeviceDependentResources()
 	return SUCCEEDED(hr);
 }
 
-
+// FrameWnd::WndProc is a static member function, so there's no 'this' pointer in the 
+//list of arguments. We can specify 'this' pointer to the last parameter of CreateWindowEx
+//And get 'this' pointer in WM_CREATE event and save it to 
 LRESULT CALLBACK FrameWnd::WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 {
 	LRESULT result = 0;
@@ -150,23 +181,18 @@ LRESULT CALLBACK FrameWnd::WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPAR
 		LPCREATESTRUCT lpCreateStruct = (LPCREATESTRUCT)lParam;
 		FrameWnd* pDemoApplication = (FrameWnd*)lpCreateStruct->lpCreateParams;
 
-		// 把this指针保存到用户自定义数据区域.
+		// Save 'this' pointer to extra window memory
 		::SetWindowLongPtr(hWnd, GWLP_USERDATA, PtrToLong(pDemoApplication));
 
-		// 设置分层属性  
-		SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);  
-		// 设置透明色   
+		// set 'WS_EX_LAYERED' style
+		SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);      
 		SetLayeredWindowAttributes(hWnd, RGB(0, 0, 0), 0, LWA_COLORKEY);   
 		result = 1;
 	}
 	else
 	{
-		// 从客户自定义数据区中取出this指针.
-		FrameWnd* pDemoApp = reinterpret_cast<FrameWnd*>(
-			static_cast<LONG_PTR>(::GetWindowLongPtr(
-			hWnd,
-			GWLP_USERDATA
-			)));
+		// get 'this' pointer from extra window memory.
+		FrameWnd* pDemoApp = reinterpret_cast<FrameWnd*>(static_cast<LONG_PTR>(::GetWindowLongPtr(hWnd, GWLP_USERDATA)));
 
 		result = 0;
 		BOOL bMsgHandled = FALSE;
@@ -175,33 +201,29 @@ LRESULT CALLBACK FrameWnd::WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPAR
 		{
 			switch(uMessage)
 			{
-			case WM_ERASEBKGND: //在窗口背景中直接贴图  
+			case WM_ERASEBKGND: //draw bitmap on the background of the window  
 				{  
-					return TRUE;  
+					//return TRUE;  
 				}  
 			case WM_SIZE:
-				/*pDemoApp->OnResize(
+				pDemoApp->OnResize(
 					LOWORD(lParam),
 					HIWORD(lParam)
-					);*/
+					);/**/
 				bMsgHandled = TRUE;
 				break;
 
 			case WM_DISPLAYCHANGE:
-				InvalidateRect(
-					hWnd,
-					NULL,
-					FALSE
-					);		//使客户区无效以致重绘.
+				//add a rectangle to the window's update region,so that 
+				//the window's client area can be redrawn
+				InvalidateRect(hWnd, NULL, FALSE);
 				bMsgHandled = TRUE;
 				break;
 
 			case WM_PAINT:
-				//pDemoApp->OnRender();
-				ValidateRect(
-					hWnd,
-					NULL
-					);		//客户区重绘完成后设为有效.
+				pDemoApp->OnRender();
+				//remove the rectangle from client area after being drawn
+				ValidateRect(hWnd, NULL);
 				bMsgHandled = TRUE;
 				break;
 			case WM_DESTROY:
@@ -217,14 +239,7 @@ LRESULT CALLBACK FrameWnd::WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPAR
 		}
 
 		if(!bMsgHandled)
-		{
-			return DefWindowProc(
-				hWnd,
-				uMessage,
-				wParam,
-				lParam
-				);
-		}
+			return DefWindowProc(hWnd, uMessage, wParam, lParam);
 	}
 
 	return result;
@@ -242,7 +257,6 @@ ResManager resManager(L"I:\\UIEngine\\docs");
 RPicture* pic = NULL;
 void FrameWnd::OnRender()
 {
-	// 检查设备是否丢失,如果丢失则需要重新创建.
 	if(!m_pRenderTarget)
 	{
 		return;
@@ -250,18 +264,15 @@ void FrameWnd::OnRender()
 
 	HRESULT hr = NULL;
 
-	//初始化绘制信息.
-	/*m_pRenderTarget->BeginDraw();
-	m_pRenderTarget->SetTransform(Matrix3x2F::Identity());
-	m_pRenderTarget->Clear(ColorF(ColorF::Black));*/
-
+	//Initialize the render target.
 	m_pDCRenderTarget->BeginDraw();
 	m_pDCRenderTarget->SetTransform(Matrix3x2F::Identity());
 	m_pDCRenderTarget->Clear(ColorF(ColorF::Black));
 
-	D2D1_SIZE_F size = m_pRenderTarget->GetSize();
-	int iWidth  = static_cast<int>(size.width);
-	int iHeight = static_cast<int>(size.height);
+	RECT dcRect;
+	HDC windowDC = GetWindowDC(m_hWnd);
+	GetWindowRect(m_hWnd, &dcRect);
+	m_pDCRenderTarget->BindDC(windowDC, &dcRect);
 
 	D2D1_RECT_F rc = RectF(130.0f, 10.0f, 230.0f, 110.0f);
 	m_pSolidBrush->SetColor(ColorF(0.0f,0.7f,0.1f,0.5f));
@@ -269,10 +280,10 @@ void FrameWnd::OnRender()
 
 	if (NULL == pic)
 	{
-		RESERROR resErr = resManager.GetResPicHandle("texturelist.Nine.btnbkg.4", &pic);
-		if (RES_SUCCESS == resErr)/**/
+		//RESERROR resErr = resManager.GetResPicHandle("texturelist.Nine.btnbkg.4", &pic);
+		//if (RES_SUCCESS == resErr)/**/
 		{
-			//pic = new RTexture(L"I:\\UIEngine\\docs\\texture.NineInOne.wndbkg_shadow.png", "texture.NineInOne.wndbkg_shadow");
+			pic = new RTexture(L"I:\\UIEngine\\docs\\texture.NineInOne.wndbkg_shadow.png", "texture.NineInOne.wndbkg_shadow");
 			//pic = new RTexture(L"I:\\UIEngine\\docs\\texture.ThreeV.xBtnBkg_Hover.png", "texture.ThreeV.xBtnBkg_Hover");
 			//pic = new RTexture(L"I:\\UIEngine\\docs\\texture.ThreeH.listctrl_scroll_bkg.png", "texture.ThreeH.listctrl_scroll_bkg");
 			//pic = new RImage(L"I:\\UIEngine\\docs\\image.settingIcon.png", "image.settingIcon");
@@ -282,35 +293,48 @@ void FrameWnd::OnRender()
 	//pic->Draw(m_pRenderTarget, 120, 210, 340, 440);
 	pic->Draw(m_pDCRenderTarget, 120, 210, 340, 440);
 
-	//结束绘制.
-	//hr = m_pRenderTarget->EndDraw();
-	
-
-	POINT sourcePosition = {};
-	POINT windowPosition = {};
-	SIZE  dcSize = { 480, 480 };
+	//POINT sourcePosition = {};
+	//POINT windowPosition = {};
+	//SIZE  dcSize = { 480, 480 };
 
 	BLENDFUNCTION blend = {};
 	blend.SourceConstantAlpha = 0;
 	blend.AlphaFormat = AC_SRC_ALPHA;
 
-	UPDATELAYEREDWINDOWINFO info = {};
-	info.cbSize = sizeof(UPDATELAYEREDWINDOWINFO);
-	info.pptSrc = &sourcePosition;
-	info.pptDst = &windowPosition;
-	info.psize = &dcSize;
-	info.pblend = &blend;
-	info.dwFlags = ULW_ALPHA;
-	info.hdcSrc = GetWindowDC(m_hWnd);
+	//UPDATELAYEREDWINDOWINFO info = {};
+	//info.cbSize = sizeof(UPDATELAYEREDWINDOWINFO);
+	//info.pptSrc = NULL;//&sourcePosition;
+	//info.pptDst = NULL;//&windowPosition;
+	//info.psize = NULL;//&dcSize;
+	//info.pblend = &blend;
+	//info.dwFlags = ULW_ALPHA; 
+	//info.crKey = RGB(0,0,0); 
+	//info.dwFlags = ULW_COLORKEY;
+    //nfo.hdcSrc = GetWindowDC(m_hWnd);
+	//info.hdcSrc = m_hWndDC;
 	
-	UpdateLayeredWindowIndirect(m_hWnd, &info);
+	//UpdateLayeredWindowIndirect(m_hWnd, &info);
 
+	//HDC dstDC = GetWindowDC(m_hWnd);
+	//HDC srcDC = CreateCompatibleDC(m_hWndDC);
+	//UpdateLayeredWindow(m_hWnd, NULL/*hdcDst*/, NULL/*pptDst*/, NULL/*psize*/, srcDC, NULL/*pptSrc*/, RGB(0,0,0), &blend, ULW_COLORKEY );
+
+	//Rendering finish.
 	hr = m_pDCRenderTarget->EndDraw();
 
+	RECT rcWin = {0};
+	GetWindowRect(m_hWnd,&rcWin);
+	POINT ptw = {rcWin.left,rcWin.top};
+	SIZE pts = {rcWin.right-rcWin.left,rcWin.bottom-rcWin.top};
+	POINT ptsrc = {0};
+	HDC ScreenDC = GetDC(0);
+	UpdateLayeredWindow(m_hWnd, ScreenDC, &ptw, &pts, MemDC, &ptsrc, 0, &blend, ULW_ALPHA);
+
+	ReleaseDC(0,ScreenDC);
 	//RenderTargetDC dc(m_interopTarget);
-	// 检查设备,如果丢失,则丢弃以备下次渲染时重新创建.
+	
 	if(hr == D2DERR_RECREATE_TARGET)
-	{
+	{// The caller needs to re-create the render target then attempt to render the frame again
 		SafeRelease(m_pD2DFactory);
 	}
 }
--- UIEngine/source/core/UIFrameWnd.h
+++ UIEngine/source/core/UIFrameWnd.h
@@ -20,10 +20,8 @@ public:
 		SafeRelease(m_pD2DFactory);
 		SafeRelease(m_pRenderTarget);
 	};
-	bool	Initialize(HINSTANCE hInstance);
-	//
-	// 消息循环.
-	//
+	bool Initialize(HINSTANCE hInstance);
+	
 	void RunMessageLoop()
 	{
 		MSG message;
@@ -35,29 +33,24 @@ public:
 		}
 	}
 protected:
-	BOOL CreateDeviceIndependentResources();		//创建设备无关资源.
-	BOOL CreateDeviceDependentResources();			//创建设备相关资源.
+	BOOL CreateDeviceIndependentResources();
+	BOOL CreateDeviceDependentResources();
 
 	void	OnRender();
 	void	OnResize(UINT uWidth, UINT uHeight);
-	static  LRESULT CALLBACK WndProc(	//消息过程函数.
-		HWND   hWnd,
-		UINT   uMessage,
-		WPARAM wParam,
-		LPARAM lParam
-		);
+	static  LRESULT CALLBACK WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam);
 private:
-	HWND						m_hWnd;				//窗口句柄.
+	HWND						m_hWnd;
 	HDC 						m_hWndDC;
-	ID2D1Factory*				m_pD2DFactory;		//D2D对象工厂.
-	ID2D1HwndRenderTarget*		m_pRenderTarget;	//绘制目标区域.
+	ID2D1Factory*				m_pD2DFactory;
+	ID2D1HwndRenderTarget*		m_pRenderTarget;
 
 	//use ID2D1DCRenderTarget when drawing on the window instead of 
 	//ID2D1HwndRenderTarget. if use the ID2D1HwndRenderTarget, the
 	//background of the window will not be transparent even though
 	//set the layered-style window and set colorkey with function
 	//SetLayeredWindowAttributes
-	ID2D1DCRenderTarget*		m_pDCRenderTarget;	//绘制目标区域.
+	ID2D1DCRenderTarget*		m_pDCRenderTarget;
 	//ID2D1GdiInteropRenderTarget m_pGdiRenderTarget;
 
 	ID2D1Brush* tmpBrush;
