ed4bc0c95aa224fb740c265812bb5fd306e29e5d
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2016-01-11 23:52:36 +0800

    test function MsgBox

--- UIEngine/example/luacode/test.lua
+++ UIEngine/example/luacode/test.lua
@@ -26,6 +26,19 @@ function LuaFun4Cpp(a, b)
 end
 
 function OnLoadLuaFile(a, b)
-	return SetGlobalObject(a, b) + 230
+	local info = ""
+	local count = 0
+	local tmp = {[1] = "sha", [2] = "be"}
+	for key, value in pairs(_G) do
+		if "function" ~= type(value) then
+			info = info.." key: "..tostring(key).." value: "..tostring(value).."\n"
+			count = count + 1
+		end
+	end
+	
+	local memCount = collectgarbage("count")
+	MsgBox("mem cost: "tostring(memCount).."KB  functions count: "..tostring(count).."\n"..info)
+	return 1
+	--return SetGlobalObject(a, b) + 230
 end
 
